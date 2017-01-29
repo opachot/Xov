538bc419d06d3a89561b0a25a871eaa35caf5695
opachot <opachot@hotmail.com>
2017-01-29 05:58:16 -0500

    Did minor fix on tool to allow multy tile data changing. Completed my very first map. Still need to fix obstacle layer later when player layer will be defined when moving.

--- Xov_Game/Assets/Script/EDITOR_MapDataModifier.cs
+++ Xov_Game/Assets/Script/EDITOR_MapDataModifier.cs
@@ -7,9 +7,9 @@ public class EDITOR_MapDataModifier : MonoBehaviour {
 
     #region DECLARATION
     // CONST
-    Color WALKABLE_COLOR = new Color(0, 255, 0, 100);
-    Color HOLE_COLOR     = new Color(0, 0, 255, 100);
-    Color OBSTACLE_COLOR = new Color(255, 0, 0, 100);
+    Color WALKABLE_COLOR = new Color(0, 1, 0, 0.3f);
+    Color HOLE_COLOR     = new Color(0, 0, 1, 0.3f);
+    Color OBSTACLE_COLOR = new Color(1, 0, 0, 0.3f);
 
     // PRIVATE
 
@@ -31,31 +31,34 @@ public class EDITOR_MapDataModifier : MonoBehaviour {
 
     void OnSceneGui(SceneView view)
     {
-        GameObject tile = Selection.activeGameObject;
+        GameObject[] tiles = Selection.gameObjects;
 
-        if (tile != null)
+        if (tiles != null)
         {
-            if (Event.current.type == EventType.keyDown)
+            if (Event.current.type == EventType.scrollWheel)
             {
-                if (tile.CompareTag("UNKNOWN"))
+                for (int i = 0; i < tiles.Length; i++)
                 {
-                    tile.tag = "WALKABLE";
-                    tile.GetComponent<SpriteRenderer>().color = WALKABLE_COLOR;
-                }
-                else if (tile.CompareTag("WALKABLE"))
-                {
-                    tile.tag = "HOLE";
-                    tile.GetComponent<SpriteRenderer>().color = HOLE_COLOR;
-                }
-                else if (tile.CompareTag("HOLE"))
-                {
-                    tile.tag = "OBSTACLE";
-                    tile.GetComponent<SpriteRenderer>().color = OBSTACLE_COLOR;
-                }
-                else if (tile.CompareTag("OBSTACLE"))
-                {
-                    tile.tag = "WALKABLE";
-                    tile.GetComponent<SpriteRenderer>().color = WALKABLE_COLOR;
+                    if (tiles[i].CompareTag("UNKNOWN"))
+                    {
+                        tiles[i].tag = "WALKABLE";
+                        tiles[i].GetComponent<SpriteRenderer>().color = WALKABLE_COLOR;
+                    }
+                    else if (tiles[i].CompareTag("WALKABLE"))
+                    {
+                        tiles[i].tag = "HOLE";
+                        tiles[i].GetComponent<SpriteRenderer>().color = HOLE_COLOR;
+                    }
+                    else if (tiles[i].CompareTag("HOLE"))
+                    {
+                        tiles[i].tag = "OBSTACLE";
+                        tiles[i].GetComponent<SpriteRenderer>().color = OBSTACLE_COLOR;
+                    }
+                    else if (tiles[i].CompareTag("OBSTACLE"))
+                    {
+                        tiles[i].tag = "WALKABLE";
+                        tiles[i].GetComponent<SpriteRenderer>().color = WALKABLE_COLOR;
+                    }
                 }
             }
         }
